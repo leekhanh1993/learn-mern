9b0337bfab12de89b245d3a90ff54a0913d8eec3
leekhanh1993 <37920433+leekhanh1993@users.noreply.github.com>
2018-08-17 22:07:14 +0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import NavigationBar from './components/NavigationBar';
+import ShoppingList from './components/ShoppingList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavigationBar/>
+        <ShoppingList/>
       </div>
     );
   }
